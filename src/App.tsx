c163a24568d382839af4947b0d92cd7d755eb9e6
Sadykov Arsen <LoCo10291998@gmail.com>
2025-01-10 03:54:52 +0600

    Second Commit. created an array of Ingredients. Uploaded pictures for the burger.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css'
+import meat from './assets/meat.png'
+import cheese from './assets/cheese.png'
+import bacon from './assets/bacon.png'
+import salad from './assets/salad.png'
+import {Ingredients} from "./types";
+import {useState} from "react";
 
 const App = () => {
+  const INGREDIENTS: Ingredients[] = [
+    {name: 'Meat', price: 80, image: meat},
+    {name: 'Cheese', price: 50, image: cheese},
+    {name: 'Bacon', price: 60, image: bacon},
+    {name: 'Salad', price: 10, image: salad},
+  ];
+
+  const [ingredients, setIngredients] = useState([
+      {name: 'Meat', count: 0},
+      {name: 'Cheese', count: 0},
+      {name: 'Bacon', count: 0},
+      {name: 'Salad', count: 0},
+  ]);
 
   return (
-    <>
 
-    </>
   )
 };
 
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export interface Ingredients{
+    name: string;
+    price: number;
+    image: string;
+}
